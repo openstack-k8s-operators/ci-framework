09b596fddad3d59b3930cac83c6452a078ceb380
Cédric Jeanneret <cjeanner@redhat.com>
2023-11-20 10:46:28 +0100

    Correct bashism in script

--- scripts/link_roles_doc.sh
+++ scripts/link_roles_doc.sh
@@ -2,7 +2,7 @@
 set -xe
 # Create links for roles' README.md in
 # docs/source/roles
-PROJECT_DIR="$(dirname $(readlink -f ${BASH_SOURCE[0]}))/../"
+PROJECT_DIR="$(dirname $(readlink -f $0))/../"
 for i in ${PROJECT_DIR}/ci_framework/roles/*/README.md; do
     dir_name=$(dirname ${i})
     role_name=$(basename ${dir_name})
