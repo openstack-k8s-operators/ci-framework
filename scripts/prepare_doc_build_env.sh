1ef81ef953410ad1531bbb57d69bf9b07c151879
Pablo Rodriguez Nava <pabrodri@redhat.com>
2023-12-13 20:05:25 +0100

    Fix the ci-framework collection

--- scripts/prepare_doc_build_env.sh
+++ scripts/prepare_doc_build_env.sh
@@ -6,14 +6,14 @@ PROJECT_DIR="$(dirname $(readlink -f $0))/../"
 # Create a symlink for ansible_collections python package
 SITE_PACKAGES=$(python -c "import site; print(site.getsitepackages()[0])")
 mkdir -p "${SITE_PACKAGES}/ansible_collections/cifmw"
-ln -nsfr ${PROJECT_DIR}/ci_framework ${SITE_PACKAGES}/ansible_collections/cifmw/general
+ln -nsfr ${PROJECT_DIR} ${SITE_PACKAGES}/ansible_collections/cifmw/general
 
 # Create links for roles' README.md in
 # docs/source/roles
-for i in ${PROJECT_DIR}/ci_framework/roles/*/README.md; do
+for i in ${PROJECT_DIR}/roles/*/README.md; do
     dir_name=$(dirname ${i})
     role_name=$(basename ${dir_name})
     test -L docs/source/roles/${role_name}.md || \
-    ln -s ../../../ci_framework/roles/${role_name}/README.md \
+    ln -s ../../../roles/${role_name}/README.md \
         docs/source/roles/${role_name}.md
 done
