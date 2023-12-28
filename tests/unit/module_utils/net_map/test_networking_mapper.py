42c86931a424ddd01d3d89bd846bc587987bd91c
Pablo Rodriguez Nava <pabrodri@redhat.com>
2023-12-28 13:53:57 +0100

    [networking_mapping] Fix network tools mapping

--- tests/unit/module_utils/net_map/test_networking_mapper.py
+++ tests/unit/module_utils/net_map/test_networking_mapper.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-
 import pytest
 from ansible_collections.cifmw.general.plugins.module_utils.net_map import (
     networking_mapper,
