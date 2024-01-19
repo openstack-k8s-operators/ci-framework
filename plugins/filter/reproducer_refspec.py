3d3423727a51a97958b36176f0ef1b7da12bde8d
jgilaber <jgilaber@redhat.com>
2024-01-19 13:18:23 +0100

    Fix refspec, url for cloning projects in reproducer
    Zuul reproducer does only seem to support cloning projects from github,
    while trying to clone projects from gerrit it uses a wrong url and
    refspec. This change introduces two filters to get the right values for
    url and refspec for the different sources (github and gerrit).

---
+++ plugins/filter/reproducer_refspec.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+__metaclass__ = type
+
+DOCUMENTATION = """
+    name: reproducer_refspec
+    short_description: Maps repo information from zuul to a refspec.
+    description:
+        - Maps repo information from zuul to a refspec that should be
+        used for pulling it with git.
+    options:
+        _input:
+            description:
+                - repo information from zuul variables.
+            type: dict
+            required: true
+"""
+
+EXAMPLES = """
+    - name: Get refspec to clone repo from rdo gerrit
+      ansible.builtin.set_fact:
+        change_refspec: >-
+            {{
+              repo | reproducer_refspec
+            }}
+"""
+
+RETURN = """
+  _value:
+    description: The refspec to pull the change.
+    type: str
+    sample:
+      "refs/changes/15/449415/5"
+
+"""
+
+from ansible.errors import AnsibleFilterError, AnsibleFilterTypeError
+
+
+class FilterModule:
+    @classmethod
+    def __map_repo_refspec(cls, repo):
+        if not isinstance(repo, dict):
+            raise AnsibleFilterTypeError(
+                f"reproducer_refspec requires a dict, got {type(repo)}"
+            )
+        if "change" not in repo:
+            # handle case pointing to main/master, e.g. a periodic job
+            return ""
+        change = repo["change"]
+        if "project" not in repo:
+            raise AnsibleFilterError(
+                "repo information does not contain 'project' field"
+            )
+        if "canonical_hostname" not in repo["project"]:
+            raise AnsibleFilterError(
+                "repo information does not contain 'canonical_hostname' field"
+            )
+        hostname = repo["project"]["canonical_hostname"]
+        if "rdoproject" in hostname or "code.eng" in hostname:
+            if "patchset" not in repo:
+                raise AnsibleFilterError(
+                    "repo information does not contain 'patchset' field"
+                )
+            patchset = repo["patchset"]
+            # changes coming from gerrit
+            return f"refs/changes/{change[-2:]}/{change}/{patchset}"
+        else:
+            # changes coming from github
+            return f"pull/{change}/head"
+
+    def filters(self):
+        return {
+            "reproducer_refspec": self.__map_repo_refspec,
+        }
