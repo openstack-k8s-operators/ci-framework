f7113dd648756dc1efeee9deac6552e1891302e2
Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>
2024-05-27 13:41:23 +0530

    [devscripts] Configure volume-group only for specific disks
    This patch changes the current behavior of picking all extra
    disks attached to OCP nodes for creating the cinder-volumes
    volume group.
    
    Signed-off-by: Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>

--- roles/devscripts/files/lv-cinder-volumes.sh
+++ roles/devscripts/files/lv-cinder-volumes.sh
@@ -1,15 +1,15 @@
 #! /usr/bin/bash
 set -euo pipefail
 
+_disks=${OSP_CINDER_VOL_DISKS:-''}
+
 if [[ $(vgdisplay cinder-volumes) ]]; then
     echo "cinder-volumes vg exists."
     exit 0
 fi
 
-disks=$(lsblk -o NAME,TYPE | awk '{ if ($2 == "disk" && $1 != "sda") print "/dev/"$1}')
 disk_str=''
-
-for disk in ${disks}; do
+for disk in ${_disks}; do
     pvcreate ${disk}
     disk_str="${disk_str} ${disk}"
 done
