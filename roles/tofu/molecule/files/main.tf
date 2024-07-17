8dfe5abcd28c45f2a0de149dfb865120e0ee7d25
Pablo Rodriguez Nava <pabrodri@redhat.com>
2024-07-17 12:12:23 +0200

    Update the tofu molecule to make it pass

--- roles/tofu/molecule/files/main.tf
+++ roles/tofu/molecule/files/main.tf
@@ -1,32 +1,48 @@
-provider "openshift" {}
-
-resource "openshift_deployment_config" "test" {
-  metadata {
-    name = "Tofu-example"
-    labels = {
-      test = "MyExampleApp"
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.23.0"
     }
   }
+}
 
+provider "kubernetes" {
+ config_path = "~/.crc/machines/crc/kubeconfig"
+}
+
+resource "kubernetes_namespace" "tofu-tests" {
+  metadata {
+    name = "tofu-experiements"
+  }
+}
+
+resource "kubernetes_deployment" "ubuntu" {
+  metadata {
+    name = "ubuntu"
+    namespace = kubernetes_namespace.tofu-tests.metadata.0.name
+  }
   spec {
     replicas = 3
-
+    selector {
+      match_labels = {
+        app = "ubuntu"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "ubuntu"
+        }
+      }
       spec {
         container {
-          image = "nginx:1.7.8"
-          name  = "example"
-
-          resources {
-            limits {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests {
-              cpu    = "250m"
-              memory = "50Mi"
-                }
-            }
-            }
+          name  = "ubuntu"
+          image = "ubuntu:latest"
+          command = ["/bin/bash", "-c", "--" ]
+          args = [ "while true; do sleep 30; done;" ]
         }
+      }
     }
+  }
 }
