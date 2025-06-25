4971e1389a5a78e12514c7054b1636fcf425c49e
Daniel Pawlik <dpawlik@redhat.com>
2025-06-25 14:33:44 +0200

    Improve crawl_n_mask script
    The script have few issues that gets mostly timeout in the CI jobs.
    
    Signed-off-by: Daniel Pawlik <dpawlik@redhat.com>

--- tests/unit/modules/test_crawl_n_mask.py
+++ tests/unit/modules/test_crawl_n_mask.py
@@ -1,8 +1,5 @@
-from xmlrpc.client import Fault
-
 import pytest
-from unittest.mock import patch, MagicMock, mock_open
-
+from unittest.mock import patch, MagicMock
 from plugins.modules import crawl_n_mask as cnm
 
 
@@ -44,51 +41,25 @@ class TestCrawlNMask:
             changed = cnm.crawl(module, test_dir)
             assert not changed
 
-    @patch("builtins.open", new_callable=mock_open, read_data="key: value")
-    @patch("yaml.safe_load_all")
-    def test_read_yaml_success(self, mock_load, mock_open_file):
-        mock_load.return_value = [{"key": "value"}]
-        module = MagicMock()
-        result = cnm.read_yaml(module, "/fake/file.yaml")
-        assert result == [{"key": "value"}]
+    def test_partial_mask_scenario_1(self):
+        example_value = " 'test1234'\n"
+        expected_value = " 'te**********34'\n"
+        test_value = cnm.partial_mask(example_value)
+        assert expected_value == test_value
 
-    def test_apply_regex(self):
-        value = "password=supersecret"
-        masked = cnm.apply_regex(value)
-        assert cnm.MASK_STR in masked
+    def test_partial_mask_scenario_2(self):
+        example_value = " osp_ci_framework_keytab\n"
+        expected_value = " 'os**********ab'\n"
+        test_value = cnm.partial_mask(example_value)
+        assert expected_value == test_value
 
-    def test_apply_regex_no_match(self):
-        value = "normal=stuff"
-        result = cnm.apply_regex(value)
-        assert result == value
+    def test_partial_mask_scenario_3(self):
+        example_value = " ''\n"
+        test_value = cnm.partial_mask(example_value)
+        assert test_value is None
 
-    @pytest.mark.parametrize(
-        "data, ismasked",
-        [
-            ([{"password": "secret"}], True),
-            ([{"secret": "value"}], True),
-            ([{True: "test_bool_key"}], False),
-            ([{1: "int_key"}], False),
-            ([{1.1: "float_key"}], False),
-        ],
-    )
-    def test_process_list(self, data, ismasked):
-        cnm.process_list(data)
-        if ismasked:
-            assert cnm.MASK_STR in [list(item.values())[0] for item in data]
-        else:
-            assert cnm.MASK_STR not in [list(item.values())[0] for item in data]
-
-    def test_apply_mask(self):
-        data = {"password": "secret", "normal": "data"}
-        cnm.apply_mask(data)
-        assert data["password"] == cnm.MASK_STR
-
-    @patch("plugins.modules.crawl_n_mask.read_yaml")
-    @patch("plugins.modules.crawl_n_mask.write_yaml")
-    def test_mask_yaml(self, mock_write, mock_read):
-        mock_read.return_value = [{"password": "secret"}]
-        mock_write.return_value = True
-        module = MagicMock()
-        changed = cnm.mask_yaml(module, "/fake/file.yaml")
-        assert changed
+    def test_partial_mask_scenario_4(self):
+        example_value = "tet"
+        expected_value = "'te**********'"
+        test_value = cnm.partial_mask(example_value)
+        assert expected_value == test_value
